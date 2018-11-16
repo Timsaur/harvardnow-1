78d10eb5b7881f8cb53a4ab0b7ef9be6f2e203d5
Eddie Tu <Yuchentu@Eddies-MacBook-Pro-2.local>
2018-11-15 19:19:43 -0500

    Tim and Eddie collaborated project

--- services/test/test.py
+++ services/test/test.py
@@ -9,7 +9,8 @@ import random
 def getJoke(input):
 
     try:
-        jokes = ["Can a kangaroo jump higher than a house? Of course, a house doesn’t jump at all.", "Anton, do you think I'm a bad mother?  My name is Paul.", "My dog used to chase people on a bike a lot. It got so bad, finally I had to take his bike away."]
+        jokes = ["Can a kangaroo jump higher than a house? Of course, a house doesn’t jump at all.", "Anton, do you think I'm a bad mother?  My name is Paul.", "My dog used to chase people on a bike a lot. It got so bad, finally I had to take his bike away.",
+        		  "A ham sandwich walks into a bar and orders a beer, the bartender says 'sorry, we don't serve food here.'"]
         joke = jokes[random.randint(0,len(jokes)-1)]
     except Exception, e:
         print str(e)
